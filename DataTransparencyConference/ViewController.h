251f42e328fcfbe3855fdc92e4fa8e74e7c4f2bf
Weien Wang <weienw@gmail.com>
2013-07-26 21:24:40 -0500

    Initial Commit

---
+++ DataTransparencyConference/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DataTransparencyConference
+//
+//  Created by Weien on 7/26/13.
+//  Copyright (c) 2013 Weien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
