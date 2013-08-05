5f6b237695578010c5169edbfe9c176c1a2be1cc
Weien Wang <weienw@gmail.com>
2013-08-05 11:49:08 -0500

    [ADD] new research project: JavascriptMapDisplaySpike

---
+++ research/JavascriptMapDisplaySpike/JavascriptMapDisplaySpike/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JavascriptMapDisplaySpike
+//
+//  Created by Weien on 8/5/13.
+//  Copyright (c) 2013 Weien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ research/JavascriptMapDisplaySpike/JavascriptMapDisplaySpike/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  JavascriptMapDisplaySpike
+//
+//  Created by Weien on 8/5/13.
+//  Copyright (c) 2013 Weien. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
