105ae6ba12512ec45c02ad70945c97031a690f7e
Weien Wang <weienw@gmail.com>
2013-08-05 15:45:21 -0500

    [NEW] ZipDownloader class (based on ZipDownloadSpike), along with project files (libz.dylib, SSZipArchive files)

---
+++ DataTransparencyConference/ZipDownloader.h
@@ -0,0 +1,13 @@
+//
+//  ZipDownloader.h
+//  DataTransparencyConference
+//
+//  Created by Weien on 8/5/13.
+//  Copyright (c) 2013 Weien. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZipDownloader : NSObject
+
+@end
