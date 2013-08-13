e8c0d80cf7560d0066dc257ab56dae358a54a73c
Weien Wang <weienw@gmail.com>
2013-08-13 12:37:50 -0400

    [ADD] UpdateFetcher singleton(?) for possible future use -- currently unused

---
+++ DataTransparencyConference/UpdateFetcher.h
@@ -0,0 +1,13 @@
+//
+//  UpdateFetcher.h
+//  DataTransparencyConference
+//
+//  Created by Weien on 8/13/13.
+//  Copyright (c) 2013 Weien. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UpdateFetcher : NSObject
+- (void) fetchUpdate;
+@end
