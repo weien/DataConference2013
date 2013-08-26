3da38b7d97b2fca2d780e23616913e82adc2b870
Weien Wang <weienw@gmail.com>
2013-08-26 16:04:26 -0500

    Restructure info view so that it contains info label, which contains dismiss button. Update layout, look, and feel.

--- DataTransparencyConference/DataMapViewController.m
+++ DataTransparencyConference/DataMapViewController.m
@@ -10,40 +10,58 @@
 #import <QuartzCore/QuartzCore.h>
 
 @interface DataMapViewController ()
-@property (strong, nonatomic) UILabel* infoView;
+@property (strong, nonatomic) UIView* infoView;
 @property (strong, nonatomic) UIButton* dismissButton;
+@property (strong, nonatomic) UILabel* infoLabel;
 
 @end
 
 @implementation DataMapViewController
 @synthesize infoView = _infoView;
 @synthesize dismissButton = _dismissButton;
+@synthesize infoLabel = _infoLabel;
 
 - (void) showInfoView {
     CGRect infoViewFrame = self.view.bounds;
-    CGRect dismissButtonFrame = CGRectMake(self.view.bounds.size.width / 2 - 20, self.view.bounds.size.height * .50, 40, 30);
+    CGRect infoLabelFrame = CGRectMake(self.view.bounds.size.width / 2 - 140, self.view.bounds.size.height * .1, 280, 140);
+//    CGRect dismissButtonFrame = CGRectMake(self.view.bounds.size.width / 2 - 20, self.view.bounds.size.height * .5, 40, 30);
+    CGRect dismissButtonFrame = CGRectMake(120, 100, 40, 30);
+
+    
+    
     if (!self.infoView) {
-//        CGPoint superViewCenter = self.view.center;
-//        CGRect infoViewFrame = CGRectMake(superViewCenter.x/2, superViewCenter.y/3, superViewCenter.x, superViewCenter.y/2);
-        self.infoView = [[UILabel alloc] initWithFrame:infoViewFrame];
+        self.infoView = [[UIView alloc] initWithFrame:infoViewFrame];
         self.infoView.backgroundColor = [UIColor colorWithRed:230/255.0f green:230/255.0f blue:230/255.0f alpha:1.0f];
-        self.infoView.numberOfLines = 0;
+//        self.infoView.numberOfLines = 0;
         self.infoView.alpha = .8;
         self.infoView.userInteractionEnabled = YES;
         
-        self.infoView.font = [UIFont boldSystemFontOfSize:16];
-        NSMutableAttributedString* formattedText = [[NSMutableAttributedString alloc] initWithString:@"This basic map of contracts for Hurricane Sandy relief awards demonstrates what the DATA Act will make possible for all federal funds. \n \n \n \n \n"];
+//        self.infoView.font = [UIFont boldSystemFontOfSize:16];
+//        NSMutableAttributedString* formattedText = [[NSMutableAttributedString alloc] initWithString:@"This basic map of Hurricane Sandy relief awards demonstrates what the DATA Act will make possible for all federal funds. \n \n \n \n \n"];
 //        [formattedText addAttribute:<#(NSString *)#> value:<#(id)#> range:<#(NSRange)#>
-        
-        self.infoView.attributedText = formattedText;
-        self.infoView.textAlignment = NSTextAlignmentCenter;
-
-        
-        
+//        self.infoView.attributedText = formattedText;
+//        self.infoView.textAlignment = NSTextAlignmentCenter;
 //        self.infoView.layer.cornerRadius = 5.0f;
 //        [self.infoView.layer setMasksToBounds:YES];
-        
         [self.externalLinkViewer addSubview:self.infoView];
+        
+        
+        self.infoLabel = [[UILabel alloc] initWithFrame:infoLabelFrame];
+        self.infoLabel.backgroundColor = [UIColor colorWithRed:180/255.0f green:180/255.0f blue:180/255.0f alpha:1.0f];
+        self.infoLabel.textColor = [UIColor blackColor];        
+        self.infoLabel.numberOfLines = 0;
+//        self.infoLabel.alpha = 1;
+        self.infoLabel.font = [UIFont boldSystemFontOfSize:16];
+        NSMutableAttributedString* formattedText = [[NSMutableAttributedString alloc] initWithString:@"This basic map of Hurricane Sandy relief awards demonstrates what the DATA Act will make possible for all federal funds. \n \n"];
+        self.infoLabel.attributedText = formattedText;
+        self.infoLabel.textAlignment = NSTextAlignmentCenter;
+        self.infoLabel.layer.cornerRadius = 5.0f;
+        [self.infoLabel.layer setMasksToBounds:YES];
+        self.infoLabel.userInteractionEnabled = YES;
+        [self.infoView addSubview:self.infoLabel];
+//        self.infoLabel.layer.borderWidth = 1;
+//        self.infoLabel.contentMode = UIViewContentModeTop;
+        
         
         self.dismissButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
         [self.dismissButton addTarget:self
@@ -53,21 +71,12 @@
         self.dismissButton.frame = dismissButtonFrame;
         self.dismissButton.backgroundColor = [UIColor clearColor];
         self.dismissButton.titleLabel.textColor = [UIColor blackColor];
-        [self.infoView addSubview:self.dismissButton];
+        [self.infoLabel addSubview:self.dismissButton];
     }
-    self.infoView.frame = infoViewFrame;
     self.infoView.hidden = NO;
+    self.infoView.frame = infoViewFrame;
     self.dismissButton.frame = dismissButtonFrame;
-    
-//        [self.syncBar setBackgroundColor:[UIColor colorWithRed:68/255.0f green:110/255.0f blue:143/255.0f alpha:1.0f]];
-//        
-//        [self.syncBar setText:@"UPDATING..."];
-//        [self.syncBar setTextAlignment:NSTextAlignmentCenter];
-//        [self.syncBar setTextColor:[UIColor whiteColor]];
-//        [self.syncBar setFont:[UIFont systemFontOfSize:8]];
-//        //        [self.syncBar setContentMode:UIViewContentModeBottom];
-//        
-//        [[[UIApplication sharedApplication] keyWindow] addSubview:self.syncBar];
+    self.infoLabel.frame = infoLabelFrame;
     
 }
 
@@ -101,12 +110,12 @@
     [super viewDidLoad];
     [self.externalLinkViewer loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.federaltransparency.gov/Style%20Library/GISMapping/index.html"]]];
     [self showInfoView];
+    [self addInfoButton];    
 }
 
 - (void) viewWillLayoutSubviews {
     [super viewWillLayoutSubviews];
     [self showInfoView];
-    [self addInfoButton];
 }
 
 @end
