cc816759a5a3b2393a2bc25b3655b7d3305c043a
Weien Wang <weienw@gmail.com>
2013-08-26 18:00:00 -0500

    Enable clicking on infoView (outside of infoLabel) to dismiss infoLabel

--- DataTransparencyConference/DataMapViewController.m
+++ DataTransparencyConference/DataMapViewController.m
@@ -10,7 +10,7 @@
 #import <QuartzCore/QuartzCore.h>
 
 @interface DataMapViewController ()
-@property (strong, nonatomic) UIView* infoView;
+@property (strong, nonatomic) UIButton* infoView;
 @property (strong, nonatomic) UIButton* dismissButton;
 @property (strong, nonatomic) UILabel* infoLabel;
 
@@ -26,15 +26,17 @@
     CGRect infoLabelFrame = CGRectMake(self.view.bounds.size.width / 2 - 140, self.view.bounds.size.height * .1, 280, 140);
 //    CGRect dismissButtonFrame = CGRectMake(self.view.bounds.size.width / 2 - 20, self.view.bounds.size.height * .5, 40, 30);
     CGRect dismissButtonFrame = CGRectMake(120, 100, 40, 30);
-
-    
     
     if (!self.infoView) {
-        self.infoView = [[UIView alloc] initWithFrame:infoViewFrame];
+        self.infoView = [[UIButton alloc] initWithFrame:infoViewFrame];
         self.infoView.backgroundColor = [UIColor colorWithRed:230/255.0f green:230/255.0f blue:230/255.0f alpha:1.0f];
 //        self.infoView.numberOfLines = 0;
         self.infoView.alpha = .8;
         self.infoView.userInteractionEnabled = YES;
+        
+        [self.infoView addTarget:self
+                               action:@selector(hideInfoView)
+                     forControlEvents:UIControlEventTouchUpInside];
         
 //        self.infoView.font = [UIFont boldSystemFontOfSize:16];
 //        NSMutableAttributedString* formattedText = [[NSMutableAttributedString alloc] initWithString:@"This basic map of Hurricane Sandy relief awards demonstrates what the DATA Act will make possible for all federal funds. \n \n \n \n \n"];
@@ -52,7 +54,7 @@
         self.infoLabel.numberOfLines = 0;
 //        self.infoLabel.alpha = 1;
         self.infoLabel.font = [UIFont boldSystemFontOfSize:16];
-        NSMutableAttributedString* formattedText = [[NSMutableAttributedString alloc] initWithString:@"This basic map of Hurricane Sandy relief awards demonstrates what the DATA Act will make possible for all federal funds. \n \n"];
+        NSMutableAttributedString* formattedText = [[NSMutableAttributedString alloc] initWithString:@"This map of Hurricane Sandy relief awards demonstrates what the DATA Act will make possible for all federal funds. \n \n"];
         self.infoLabel.attributedText = formattedText;
         self.infoLabel.textAlignment = NSTextAlignmentCenter;
         self.infoLabel.layer.cornerRadius = 5.0f;
