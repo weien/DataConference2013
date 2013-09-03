379d6bbb11571458e4d7f9b2eeab3246036e60d2
Weien Wang <weienw@gmail.com>
2013-09-03 15:16:22 -0500

    refactor variable names in NewsViewController, update length of time to display animation

--- DataTransparencyConference/NewsViewController.m
+++ DataTransparencyConference/NewsViewController.m
@@ -9,12 +9,12 @@
 #import "NewsViewController.h"
 
 @interface NewsViewController ()
-@property (strong, nonatomic) UIImageView* iv;
+@property (strong, nonatomic) UIImageView* launchImageView;
 @property BOOL splashScreenHasBeenShown;
 @end
 
 @implementation NewsViewController
-@synthesize iv = _iv;
+@synthesize launchImageView = _launchImageView;
 @synthesize splashScreenHasBeenShown = _splashScreenHasBeenShown;
 
 - (NSString*) uniqueTabPathComponent {
@@ -24,7 +24,7 @@
 
 - (void) webViewDidFinishLoad:(UIWebView *)webView {
     [super webViewDidFinishLoad:webView];
-    self.iv.hidden = YES;
+    self.launchImageView.hidden = YES;
 }
 
 - (void) preventBlackFlash {
@@ -34,17 +34,17 @@
     //    [self.DTCWebView loadHTMLString:[NSString stringWithFormat:@"<html><body><img src=\"file://%@\"></body></html>",path] baseURL:nil];
     
     CGRect screenBounds = [[UIScreen mainScreen] bounds];
-    self.iv = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, screenBounds.size.width, screenBounds.size.height)];
+    self.launchImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, screenBounds.size.width, screenBounds.size.height)];
 
     //thanks http://stackoverflow.com/a/12532527/2284713 -- have to manually check for 4" display
     CGFloat screenHeight = [UIScreen mainScreen].bounds.size.height;
     if ([UIScreen mainScreen].scale == 2.f && screenHeight == 568.0f) {
-        self.iv.image = [UIImage imageNamed:@"Default-568h.png"];
+        self.launchImageView.image = [UIImage imageNamed:@"Default-568h.png"];
     } else {
-        self.iv.image = [UIImage imageNamed:@"Default.png"];
+        self.launchImageView.image = [UIImage imageNamed:@"Default.png"];
     }
-    self.iv.hidden = NO;
-    [self.navigationController.view addSubview:self.iv];
+    self.launchImageView.hidden = NO;
+    [self.navigationController.view addSubview:self.launchImageView];
 }
 
 - (void) viewDidLoad {
@@ -59,24 +59,22 @@
 - (void) displaySplashScreen {
     
     if (!self.splashScreenHasBeenShown) {
-        UIImageView*imageView = nil;
-        if ([UIScreen mainScreen].bounds.size.height == 568.0f) {
-            NSLog(@"1");
-            imageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"splash_full-568h.png"]];
+        UIImageView* splashScreen = nil;
+        if ([UIScreen mainScreen].scale == 2.f && [UIScreen mainScreen].bounds.size.height == 568.0f) {
+            splashScreen = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"splash_full-568h.png"]];
         }
         else {
-            NSLog(@"2");
-            imageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"splash_full.png"]];
+            splashScreen = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"splash_full.png"]];
         }
         
         UIViewController* vc = [[[UIApplication sharedApplication] keyWindow] rootViewController];
-        [[vc view] addSubview:imageView];
-        [[vc view] bringSubviewToFront:imageView];
+        [[vc view] addSubview:splashScreen];
+        [[vc view] bringSubviewToFront:splashScreen];
         
         // as usual
         //    [self.window makeKeyAndVisible];
         
-        [UIView animateWithDuration:1.0f delay:0.5f options:UIViewAnimationOptionTransitionNone animations:^(void){imageView.alpha=0.0f;} completion:^(BOOL finished){[imageView removeFromSuperview];}];
+        [UIView animateWithDuration:0.5f delay:1.0f options:UIViewAnimationOptionTransitionNone animations:^(void){splashScreen.alpha=0.0f;} completion:^(BOOL finished){[splashScreen removeFromSuperview];}];
     }
     self.splashScreenHasBeenShown = YES;
 }
